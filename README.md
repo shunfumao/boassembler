14778fce8514ccf64c7f4726deacc062294a1f65
olivomao <olivo_mao@hotmail.com>
2019-02-15 16:47:23 -0800

    add user interface to subsample, and update README.md for this

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# boassembler
+# BOAssembler
+
+### Contents <a id='contents'></a>
+
+* <a href='#subsample'>Subsample</a>  
+
+---
+
+### Subsample a large read alignment <a id='subsample'></a>
+
+For efficiency, BOAssembler is expected to tune parameters based on a small dataset (e.g. alignment) first, and then apply parameters onto large dataset assembly tasks. To prepare sampling a small dataset from a large dataset, you can use our prepared subsample.py. 
+
+**Syntax**
+
+```
+python subsample.py -a /path/to/large/source/alignment \
+                    -o /path/to/small/destination/alignment \
+                    -chroms target_chrom_list \
+                    -covs target_coverage_list \
+                    -N_per_cov num_region_to_pick_per_coverage \
+                    -span per_region_span
+```
+
+| Argument      | Description  |
+| ------------- |:------------- |
+| -a | full path to the large source alignment |
+| -o | full path to the small destination alignment |
+|-chroms| specify the target chromosomes, connected by comma. For example to extract only chr1 and chr15, try -chroms chr1,chr15. By default, it's -chroms all|
+|-covs| specify the target coverage regions. For a region with span S bp, its coverage is defined as num_reads_aligned_in_this_region * read_len / S. For example, to extract only regions with coverage [5x, 10x) and [10x, 15x),  try -covs 5,10,15 . By default, it's -covs all|
+|-N_per_cov|Specify for each coverage level, the number of regions to sample. For example, for coverage level [5x,10x), we have regions A, B and for coverage level [10x, 15x), we have regions C, D. If we set -N_per_cov 1, we will pick one region from [5x, 10x) and one region from [10x, 15x) randomly. If a coverage level has less regions, all regions will be picked. Defaut is 10.|
+|-span| Specify the length of each region. For example, if -span 100000, each chromosome will be divided into 100000-bp regions.|
+
+**Example 1. Sample one chromosome**
+
+Let's use our prepared input alignment (data/chr1_chr15_cov_5_10_15.bam) which contains read alignments with coverage 5x~15x from chromosome 1 and chromosome 15. We hope to extract only chr15's alignments to store to (data/ex1.bam). We can use:
+
+```
+python subsample.py -a data/chr1_chr15_cov_5_10_15.bam \
+                    -o data/ex1.bam \
+                    -chroms chr15 \
+                    -covs all \
+                    -N_per_cov 5 \
+                    -span 100000
+```
+
+**Example 2. Sample multiple chromosoms and multiple coverages**
+
+Let's use our prepared input alignment (data/chr1_chr15_cov_5_10_15.bam) which contains read alignments with coverage 5x~15x from chromosome 1 and chromosome 15. We hope to extract coverage levels [5x,7x) and [7x,12x) (each coverage level at most 3 regions) from chromosome 1 and 15, and store to (data/ex2.bam). We can use:
+
+```
+python subsample.py -a data/chr1_chr15_cov_5_10_15.bam \
+                    -o data/ex2.bam \
+                    -chroms chr1,chr15 \
+                    -covs 5,7,12 \
+                    -N_per_cov 3 \
+                    -span 100000
+```
+
+
--- subsample.py
+++ subsample.py
@@ -2,6 +2,8 @@ import pysam
 import pdb
 from util import iterCounter
 import numpy as np
+import sys
+from argparse import ArgumentParser
 
 def get_dic_chrom_len(alignment):
   '''
@@ -19,20 +21,47 @@ def get_dic_chrom_len(alignment):
     
   return dic_chrom_len
 
-def analyze_alignment(alignment, output_alignment, span=100000):
+def analyze_alignment(alignment,
+                      output_alignment,
+                      target_chroms,
+                      target_coverage,
+                      N_per_cov,
+                      span):
   '''
   Input:
     bam format alignment
+    output_alignment: subsampled alignment
+    target_chroms: all or chr_a,chr_b... the chroms to subsample
+    target_coverage: all or a,b,... the coverage regions to subsample
+    N_per_cov: number of sub-regions to random sample from each coverage category.
     span: per region span
   Output:
-    dic_split: key - chrom val - {region (stt/inclusive, stp/exclusive): read counts}
+    dic_split2: key - chrom val - {region (stt/inclusive, stp/exclusive): read counts}
       per region share same length
+    output_alignment: subsampled alignments will be stored here
   '''
   dic_split = {}
 
   dic_chrom_len = get_dic_chrom_len(alignment) 
 
-  for chrom in dic_chrom_len.keys():
+  chrom_list = dic_chrom_len.keys()
+  print 'availabe chroms: %s'%str(chrom_list)
+  if target_chroms != 'all':
+    target_chroms = [c for c in target_chroms.split(',') if c!='']
+    chrom_list = [c for c in chrom_list if c in target_chroms]
+  print 'used chroms: %s'%str(chrom_list)
+
+  if target_coverage != 'all':
+    cov_list1 = [int(c) for c in target_coverage.split(',') if c!='']
+    coverage_regions = []
+    for i in range(len(cov_list1)-1):
+      coverage_regions.append((cov_list1[i], cov_list1[i+1]))
+  else:
+    coverage_regions=[(0, np.inf)]
+  print 'target coverages: %s'%str(coverage_regions)
+  #pdb.set_trace()
+
+  for chrom in chrom_list:
     dic_split[chrom] = {}
     chrom_len = dic_chrom_len[chrom]
     N = chrom_len / span + 1
@@ -40,15 +69,26 @@ def analyze_alignment(alignment, output_alignment, span=100000):
       dic_split[chrom][(i*span, (i+1)*span)]=0
 
   nlines = sum([1 for l in pysam.AlignmentFile(alignment, 'rb')])
-  print 'nlines=%d'%nlines
+  print 'source alignment has nlines=%d'%nlines
   iterCnt = iterCounter(nlines, 'process alignment (%d lines)'%nlines)
+  avg_read_len = 0
+  cnt_read = 0
   with pysam.AlignmentFile(alignment, 'rb') as f:
     for rd in f:
       iterCnt.inc()
+      if rd.reference_name not in chrom_list:
+        continue
+      #pdb.set_trace()
+      #print 'a:',' cnt_rd:',cnt_read, ' rd len:', rd.query_length, ' avg rd len:', avg_read_len
+      avg_read_len = float(cnt_read) / (cnt_read + 1) * avg_read_len + float(rd.query_length) / (cnt_read + 1)
+      avg_read_len = int(avg_read_len)
+      cnt_read += 1
+      #print 'b:', 'avg rd len (new):', avg_read_len, ' cnt rd new:', cnt_read
       reg_i = rd.pos / span
       reg = (reg_i*span, (reg_i+1)*span)
       dic_split[rd.reference_name][reg]+=1
     iterCnt.finish()
+
   '''
   region_size_list = []
   for chrom in dic_split.keys():
@@ -58,35 +98,40 @@ def analyze_alignment(alignment, output_alignment, span=100000):
   pdb.set_trace()
   '''
   #TODO(shunfu) fix cov_regions, read len and N_pick
-  cov_regions = [(5,10)]
-  for i in range(10):
-    cov_regions.append(((i+1)*10, (i+2)*10))
-  read_len = 100
-  N_pick = 15 
+  cov_regions = coverage_regions
+  read_len = avg_read_len 
+  N_pick = N_per_cov 
   cov_dic = {}
   for chrom in dic_split.keys():
     for reg, cnt in dic_split[chrom].items():
       reg_cov = cnt * read_len / span
       for cov in cov_regions:
-        if reg_cov >= cov[0] and reg_cov < cov[1]:
+        if reg_cov > cov[0] and reg_cov < cov[1]:
           cov_dic.setdefault(cov, []).append((chrom, reg[0], reg[1], cnt))
 
+  print 'cov_dic:'
+  for k in cov_dic.keys():
+    print k
+    for kk in cov_dic[k]:
+      print '  ', kk 
+
   dic_split2 = {} #key chrom val dic key region idx val cnt
-  #N_pick = 50 #each cov_regions pick at most 10 at random
+  print 'sample idx, cov category, sample idx per cov, actual idx per cov, chrom, region stt, regio stp, read cnts'
+  sample_idx = 0
   for cov in cov_dic.keys():
     list_chrom_reg0_reg1_cnt = cov_dic[cov]
     perm_idx_list = np.random.permutation(len(list_chrom_reg0_reg1_cnt))
     for i in range(min(N_pick, len(perm_idx_list))):
       idx = perm_idx_list[i]
       chrom, reg0, reg1, cnt = list_chrom_reg0_reg1_cnt[idx]
-      print cov, i, idx, chrom, reg0, reg1, cnt
+      sample_idx += 1
+      print sample_idx, cov, i, idx, chrom, reg0, reg1, cnt
       dic_split2.setdefault(chrom, {}).setdefault(reg0/span,cnt)
       
   fi = pysam.AlignmentFile(alignment, 'rb')
-  #output_alignment = alignment + '.dmp'
   fo = pysam.AlignmentFile(output_alignment, 'wb', header=fi.header)
   cnt = 0
-  iterCnt = iterCounter(nlines, 'dump %d'%nlines)
+  iterCnt = iterCounter(nlines, 'write output alignment %d'%nlines)
   for rd in fi:
     iterCnt.inc()
     chrom = rd.reference_name
@@ -94,23 +139,43 @@ def analyze_alignment(alignment, output_alignment, span=100000):
     if chrom in dic_split2 and reg_i in dic_split2[chrom]:
       fo.write(rd)
       cnt += 1
-  print '%s (%d reads)  written'%(output_alignment, cnt)
+  print '\n%s (%d reads)  written'%(output_alignment, cnt)
   iterCnt.finish()
   fi.close()
   fo.close()
   return dic_split2
 
+def main():
+
+  parser = ArgumentParser()
+  
+  parser.add_argument(
+    '-a', help='source alignment path') #args[args.index('-a')+1]
+  parser.add_argument(
+    '-o', help='destination alignment path') #args[args.index('-o')+1]
+  parser.add_argument(
+    '-chroms', default='all',
+    help='target chroms to extract. e.g. chr1,chr5,... default is all') #args[args.index('-c')+1]
+  parser.add_argument(
+    '-covs', default='all',
+    help='target coverage to extract. e.g. 2,5,10,15 refer to cov [2x,5x),[5x,10x),[10x,15x) default is all')
+  parser.add_argument(
+    '-N_per_cov', type=int, default=10,
+    help='number of sub-regions to sample from each coverage category. default is 10')
+  parser.add_argument(
+    '-span', type=int, default=10000,
+    help='specify the span of the region per chrom is divided by')
+  
+  args = parser.parse_args()
+
+  dic_split = analyze_alignment(args.a, #source_alignment,
+                                args.o, #dest_alignment,
+                                args.chroms, #target_chroms,
+                                args.covs, #target_coverage,
+                                args.N_per_cov,
+                                args.span)
+
+  return
+
 if __name__ == '__main__':
-  alignment_in_regions='/data1/shunfu1/ref_shannon_modi/data/sgRefShannon/snyderSim_1018a/hits.sorted.regions.bam'
-  #to /data1/shunfu1/ref_shannon_modi/data/sgRefShannon/snyderSim_1018a/hits.sorted.regions.subsample.bam (119M)
-
-  #alignment_in_regions='/data1/shunfu1/ref_shannon_modi/data/sgRefShannon/wwSim_1122a/hits.sorted.bam'
-
-  #alignment_in_regions='/data1/shunfu/boassembler/alignments/snyder_small/hits.sorted.bam'
-  #alignment_in_regions='small.bam'
-  
-
-  output_alignment = alignment_in_regions + '.dump.20190212.snyder.npick15'
-
-  dic_split = analyze_alignment(alignment_in_regions, output_alignment)
-  
+  main()
