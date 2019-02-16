3dc02b36f43aa8ebe87e3620ac041900ebd4b412
olivomao <olivo_mao@hotmail.com>
2019-02-15 21:26:22 -0800

    clean run.sh and bo_gpyopt.py

--- run.sh
+++
@@ -1,5 +0,0 @@
-# python bo.py --assembler_name scallop --metric_type tr-f1
-
-# python bo.py --assembler_name stringtie --metric_type tr-f1
-
-python bo.py --assembler_name stringtie --metric_type tr-f1 --case_id 20190210-small
